dec12d06c94c8c10374a5f9a6aec83c2fb366fca
onweg <innokentiydiscord@yandex.ru>
2025-10-20 18:52:45 +0300

    resolve merge conflict: remove handler_test.go

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -8,20 +8,28 @@ import (
 	"github.com/onweg/UrlShorteningService/internal/service"
 )
 
-func HandleRequest() func(res http.ResponseWriter, req *http.Request) {
+type Handler struct {
+	urlService *service.URLService
+}
+
+func NewHandler(urlService *service.URLService) *Handler {
+	return &Handler{urlService: urlService}
+}
+
+func (h *Handler) HandleRequest() func(res http.ResponseWriter, req *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
 		switch req.Method {
 		case http.MethodGet:
-			handleGet(res, req)
+			h.handleGet(res, req)
 		case http.MethodPost:
-			handlePost(res, req)
+			h.handlePost(res, req)
 		default:
 			res.Write([]byte("the service supports only GET and POST requests"))
 		}
 	}
 }
 
-func handlePost(res http.ResponseWriter, req *http.Request) {
+func (h *Handler) handlePost(res http.ResponseWriter, req *http.Request) {
 	resBody, err := io.ReadAll(req.Body)
 	defer req.Body.Close()
 	if err != nil {
@@ -31,7 +39,7 @@ func handlePost(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	shortUrl, err := service.ShorteningUrl(string(resBody))
+	shortUrl, err := h.urlService.ShorteningUrl(string(resBody))
 	if err != nil {
 		// не смог сделать ключ, вернуть 500 ошибку
 		res.WriteHeader(http.StatusBadRequest)
@@ -45,14 +53,14 @@ func handlePost(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte(outResult))
 }
 
-func handleGet(res http.ResponseWriter, req *http.Request) {
+func (h *Handler) handleGet(res http.ResponseWriter, req *http.Request) {
 	keyFindUrl := req.RequestURI[1:]
 	if keyFindUrl == "" {
 		res.Write([]byte("Добро пожаловать!"))
 		return
 	}
 
-	url, err := service.GetOriginalUrl(keyFindUrl)
+	url, err := h.urlService.GetOriginalUrl(keyFindUrl)
 	log.Printf("Get url: %s to key: %s\n", url, keyFindUrl)
 	if err != nil {
 		// некорректный id не нашли по id нужный url, вернуть 400
